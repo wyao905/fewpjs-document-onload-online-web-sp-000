fbab5e318f333ad703c274f518c85f5d8e867b0a
Warren Yao <warrenyca905@gmail.com>
2020-01-15 01:24:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded",
 function() {
-  document.getElementById()
+  document.getElementById("text").innerHTML = "This is really cool!"
 });
